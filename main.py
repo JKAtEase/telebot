6c082175aa81d1f811737acbf09194373832c461
JKAtEase <65213326+JKAtEase@users.noreply.github.com>
2021-07-13 12:10:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,20 @@ convertapi.api_secret = file_conversion_api_key
 
 bot = telebot.TeleBot(api_key)
 server = Flask(__name__)
+
+@server.route('/' + api_key, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "This are post requests.", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://gittelebot.herokuapp.com/' + api_key)
+    return "This is a get request.", 200
 server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 #Function for exiting.
 @bot.message_handler(commands=['Exit'])
@@ -121,19 +135,6 @@ def provide_functionality(message):
   except Exception as e:
         bot.reply_to(message, 'Something went wrong!')
 
-@server.route('/' + api_key, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "This are post requests.", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://gittelebot.herokuapp.com/' + api_key)
-    return "This is a get request.", 200
 
 bot.enable_save_next_step_handlers(delay=1)
 bot.load_next_step_handlers()
