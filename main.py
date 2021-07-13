f500df9cdae1ecba03e6a2961eade46a0407b240
JKAtEase <65213326+JKAtEase@users.noreply.github.com>
2021-07-13 15:37:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,13 +42,16 @@ def index():
 
 @app.route('/' + api_key, methods=['POST'])
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
+    update = request.get_json()
+    bot.process_new_updates([update])
+    return "OK"
+#     if flask.request.headers.get('content-type') == 'application/json':
+#         json_string = flask.request.get_data().decode('utf-8')
+#         update = telebot.types.Update.de_json(json_string)
+#         bot.process_new_updates([update])
+#         return ''
+#     else:
+#         flask.abort(403)
 
 bot.remove_webhook()
 
